a6426469f0071f62f6ed5abfdcb95545ffe4e194
Aaron Lista <aarondavidlista@gmail.com>
2024-02-28 16:38:23 -0400

    intento de seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Creating users..."
+
+erika = User.create(email: "erika.azuaje2014@gmail.com", password: "Patico2014",
+  first_name: "Erika", last_name: "Azuaje", nickname: "PaticoAzuaje")
+
+jose = User.create(email: "joseperalta2910@gmail.com", password: "123456",
+  first_name: "Jose", last_name: "Peralta", nickname: "Kronorit")
+
+aaron = User.create(email: "aarondlista@gmail.com", password: "holamundo2",
+  first_name: "Aaron", last_name: "Azuaje", nickname: "PaticoAzuaje")
+
+puts "3 Users created"
+
+puts 'creating vehicle for each user...'
+[erika, jose, aaron].each do |user|
+  puts "creating 3 vehicles for #{user.first_name}"
+  Vehicle.new(vehicle_type: 'coupe', seats: 2, price: 1000,
+              description: "lorem ipsum dolor", user: user)
+
+  Vehicle.new(vehicle_type: 'sedan', seats: 4, price: 2000,
+              description: "lorem ipsum dolor", user: user)
+
+  Vehicle.new(vehicle_type: 'bus', seats: 15, price: 2500,
+              description: "lorem ipsum dolor", user: user)
+end
+puts 'all vehicles created'
+
+puts 'creating rents for each vehicle'

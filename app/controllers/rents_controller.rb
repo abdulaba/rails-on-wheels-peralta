ab664d760afa68df60412380d2fbecce28b90f39
erika <eazuaje@bancomercantil.com>
2024-02-29 10:15:37 -0400

    cambios html rents

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -2,28 +2,25 @@ class RentsController < ApplicationController
   before_action :set_rent, only: [:show, :edit, :update, :destroy]
 
   def index
-    @rents = current_user.rents
+    @rents = Rent.all
   end
 
   def show
   end
 
   def new
-    @rent = Rent.new
+    @rent = @vehicle.rents.new
   end
 
   def create
-    @rent = Rent.new(rent_params)
+    @rent = @vehicle.rents.new(rent_params)
     @rent.user = current_user
 
     if @rent.save
-      redirect_to @rent, notice: 'Renta creada exitosamente.'
+      redirect_to @vehicle, notice: 'Renta creada exitosamente.'
     else
       render :new
     end
-  end
-
-  def edit
   end
 
   def update
@@ -41,12 +38,17 @@ class RentsController < ApplicationController
 
   private
 
+  def set_vehicle
+    @vehicle = Vehicle.find(params[:vehicle_id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: 'Vehículo no encontrado.'
+  end
+
   def set_rent
     @rent = Rent.find(params[:id])
   end
 
   def rent_params
-    # Define los parámetros permitidos para crear o actualizar una renta
-    params.require(:rent).permit(:vehicle_id, :start_date, :end_date, :other_params)
+    params.require(:rent).permit(:start_date, :end_date, :price)
   end
 end

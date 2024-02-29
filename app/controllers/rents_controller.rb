dc232e709cc6d087502f55e7c373c334bb17b23e
erika <eazuaje@bancomercantil.com>
2024-02-29 12:00:56 -0400

    cambios html rents

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,5 +1,6 @@
 class RentsController < ApplicationController
-  before_action :set_vehicle, only: [:index, :new, :create]
+  before_action :set_rent, only: [:show, :edit, :update, :destroy]
+  before_action :set_vehicle, only: [:new, :create]
 
   def index
     @rents = current_user.rents
@@ -9,23 +10,27 @@ class RentsController < ApplicationController
   end
 
   def new
-    @rent = @vehicle.rents.new
+    @rent = Rent.new
   end
 
   def create
     @rent = @vehicle.rents.new(rent_params)
+    @rent.price = @vehicle.price
     @rent.user = current_user
 
     if @rent.save
-      redirect_to @vehicle, notice: 'Renta creada exitosamente.'
+      redirect_to rents_path, notice: 'Renta creada exitosamente.'
     else
       render :new
     end
   end
 
+  def edit
+  end
+
   def update
     if @rent.update(rent_params)
-      redirect_to @rent, notice: 'Renta actualizada exitosamente.'
+      redirect_to rents_url, notice: 'Renta actualizada exitosamente.'
     else
       render :edit
     end
@@ -39,7 +44,7 @@ class RentsController < ApplicationController
   private
 
   def set_vehicle
-    @vehicle = Vehicle.find(params[:vehicle_id]) if params[:vehicle_id].present?
+    @vehicle = Vehicle.find(params[:vehicle_id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path, alert: 'Vehículo no encontrado.'
   end
@@ -49,6 +54,6 @@ class RentsController < ApplicationController
   end
 
   def rent_params
-    params.require(:rent).permit(:start_date, :end_date, :price)
+    params.require(:rent).permit(:start_date, :end_date)
   end
 end

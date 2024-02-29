174a367147f05e70d5c2c750184e121cdb714e2e
Aaron Lista <aarondavidlista@gmail.com>
2024-02-29 09:19:26 -0400

    todo listo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_many :vehicles
   has_many :rents
+
+  # user image
+
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

0f37c543861b94a1b4e1806640643d6aeacbb2dd
Jose Peralta <joseperalta2910@gmail.com>
2024-02-29 13:53:50 -0400

    Vista de vehículo realizada

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -9,7 +9,7 @@ class Vehicle < ApplicationRecord
   validates :user, :vehicle_type, :price, :seats, :description, :make, :model, :year, presence: true
   validates :price, inclusion: { in: (1..10_000).to_a, message: "should be a number between 1 and 10,000" }
   validates :seats, inclusion: { in: (1..50).to_a, message: "should be a number between 1 and 50" }
-  validates :description, length: { maximum: 120 }
+  validates :description, length: { minimum: 50, maximum: 120 }
   validates :make, :model, length: { maximum: 50 }
   validates :year, comparison: {
     greater_than_or_equal_to: 1960, message: "should be a year older than 1960",

90074dc93259795aee2e15e65eba261c1e287461
Aaron Lista <aarondavidlista@gmail.com>
2024-03-01 10:22:59 -0400

    seeds con fotos de vehiculos

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,25 +38,41 @@ puts "3 Users created"
 
 puts 'creating vehicle for each user...'
 
+cherokee_photo = URI.open("https://es.jeep.com/content/dam/fca-brands/na/jeep/en_us/2024/wrangler/gallery/desktop/MY24-Wrangler-Gallery-Exterior-1-Desktop.jpg.image.1440.jpg")
+optra_photo = URI.open("https://media.chevrolet.com/dld/content/dam/Media/images/EC/Vehicles/Chevrolet/automoviles/Optra/2011/2011-Chevrolet-Optra-34AE24.jpg")
+corolla_photo = URI.open("https://media.ed.edmunds-media.com/toyota/corolla/2023/oem/2023_toyota_corolla_sedan_xse_fq_oem_1_1280.jpg")
+
 [erika, jose, aaron].each do |user|
   puts "creating 3 vehicles for #{user.first_name}"
-  puts Vehicle.create(vehicle_type: 'coupe', seats: 2, price: 1000,
+  corolla = Vehicle.new(vehicle_type: 'sedan', seats: 4, price: 1000,
                  description: "Lorem ipsum dolor sit amet consectetur adipisicing
                  elit. Aut dignissimos quasi blanditiis praesentium",
                  make: "Toyota",
-                 model: "Corolla", user: user, year: 2010).valid?
+                 model: "Corolla", user: user, year: 2010)
 
-  puts Vehicle.create(vehicle_type: 'sedan', seats: 4, price: 2000,
+  corolla.photos.attach([io: corolla_photo, filename: "corolla", content_type: "image/png"])
+
+  puts corolla.save.valid?
+
+  optra = Vehicle.create(vehicle_type: 'sedan', seats: 4, price: 2000,
                  description: "Lorem ipsum dolor sit amet consectetur adipisicing
                  elit. Aut dignissimos quasi blanditiis praesentium",
                  make: "Chevrolet",
-                 model: "Optra", user: user, year: 2010).valid?
+                 model: "Optra", user: user, year: 2010)
 
-  puts Vehicle.create(vehicle_type: 'bus', seats: 15, price: 2500,
+  optra.photos.attach([io: optra_photo, filename: "optra", content_type: "image/png"])
+
+  puts optra.save.valid?
+
+  cherokee = Vehicle.create(vehicle_type: 'suv', seats: 4, price: 2500,
                  description: "Lorem ipsum dolor sit amet consectetur adipisicing
                  elit. Aut dignissimos quasi blanditiis praesentium",
                  make: "Jeep",
-                 model: "Cherokee", user: user, year: 2010).valid?
+                 model: "Cherokee", user: user, year: 2010)
+
+  cherokee.photos.attach([io: cherokee_photo, filename: "cherokee", content_type: "image/png"])
+
+  puts cherokee.save.valid?
 end
 puts 'all vehicles created'
 
